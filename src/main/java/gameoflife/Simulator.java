0b4e76d222533b459258a3d81f5c28ebdc7114e2
Víctor A. Javariz Vega <5726868-vjavariz@users.noreply.gitlab.com>
2020-08-27 20:59:51 -0500

    Create Simulator.java to add simulation "playback" functionality

---
+++ src/main/java/gameoflife/Simulator.java
@@ -0,0 +1,32 @@
+package gameoflife;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
+
+public class Simulator {
+    private Timeline timeline;
+    private View view;
+    private Board board;
+
+    public Simulator(View view, Board board) {
+        this.view = view;
+        this.board = board;
+        this.timeline = new Timeline(new KeyFrame(Duration.millis(500), this::doStep));
+    }
+
+    private void doStep(ActionEvent actionEvent) {
+        this.board.next();
+        this.view.draw();
+    }
+
+    public void start() {
+        this.timeline.play();
+    }
+
+    public void stop() {
+        this.timeline.stop();
+    }
+
+}
